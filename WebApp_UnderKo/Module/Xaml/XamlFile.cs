ff21843aeeb8e8dd668e47a8bd5f24f3a1738be8
UnderKo <UnderKo@DESKTOP-ATCVI05>
2024-06-27 22:22:38 +0300

    Добавьте файлы проекта.

---
+++ WebApp_UnderKo/Module/Xaml/XamlFile.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml.Serialization;
+
+namespace WebApp_UnderKo.Module.Xaml
+{
+    public class XamlObjFile<T>
+    {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+        public void ObjectSerialize(T data, string path)
+        {
+            try
+            {
+
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    xmlSerializer.Serialize(fs, data);
+
+                    Console.WriteLine("Object has been serialized");
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public T ObjectDeSerialize(string path)
+        {
+            T? class_ = default(T);
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    class_ = (T)xmlSerializer.Deserialize(fs);
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return class_;
+        }
+    }
+}
