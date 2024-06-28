5f33dfbbd78ef2ac64a81a6b65c3bfa428972cfc
UnderKo <UnderKo@DESKTOP-ATCVI05>
2024-06-28 03:34:53 +0300

    d

---
+++ WebApp_UnderKo/Models/JsonObjects/objIPCheckerResult.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp_UnderKo.Models.JsonObjects
+{
+    public class objIPCheckerResult
+    {
+        public string query { get; set; }
+        public string status { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string region { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string timezone { get; set; }
+        public string isp { get; set; }
+        public string org { get; set; }
+        public string @as { get; set; }
+    }
+}
