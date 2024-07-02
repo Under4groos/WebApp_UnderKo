a124e5875e3c87d948034da27d5c44c31aa557a3
UnderKo <UnderKo@DESKTOP-ATCVI05>
2024-07-02 15:33:16 +0300

    фыв

--- WebApp_UnderKo/Models/RazorPage/base_Page.cs
+++ WebApp_UnderKo/Models/RazorPage/base_Page.cs
@@ -1,59 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
 using System.Text;
-using WebApp_UnderKo.Models.JsonObjects;
-using WebApp_UnderKo.Models.Request;
 
 namespace WebApp_UnderKo.Models.RazorPage
 {
+    public static class base_IPCheker
+    {
+        public static void LogIp(this HttpContext httpContext)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (httpContext.Request.Headers.ContainsKey("X-Real-IP"))
+            {
+                string ip_str_ = httpContext.Request.Headers["X-Real-IP"];
+                stringBuilder.Append($"[{ip_str_}] ");
+            }
+            stringBuilder.Append(httpContext.Request.Method + " - ");
+            stringBuilder.Append(httpContext.Request.Path + httpContext.Request.QueryString);
+            G_.logger.NewLine($"{stringBuilder}");
+        }
+    }
     public static class base_Page
     {
         public static void Init(this PageModel pageModel)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            if (pageModel.HttpContext.Request.Headers.ContainsKey("X-Real-IP"))
-            {
-                string ip_str_ = pageModel.HttpContext.Request.Headers["X-Real-IP"];
-                stringBuilder.Append($"[{ip_str_}]");
-
-                WebReq.AsyncRequest($"http://ip-api.com/json/{ip_str_}?fields=regionName,city,as", (string url, string jsonresult) =>
-                {
-                    objIPCheckerResult myDeserializedClass = JsonConvert.DeserializeObject<objIPCheckerResult>(jsonresult);
-                    stringBuilder.Append($"[{myDeserializedClass.city}] - ");
-                });
-
-
-            }
-
-            stringBuilder.Append(pageModel.HttpContext.Request.Method + " - ");
-            stringBuilder.Append(pageModel.HttpContext.Request.Path + pageModel.HttpContext.Request.QueryString);
-            G_.logger.NewLine($"{stringBuilder}");
+            pageModel.HttpContext.LogIp();
         }
     }
     public static class base_ControllerBase
     {
-
         public static void Init(this ControllerBase controllerBase)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            if (controllerBase.HttpContext.Request.Headers.ContainsKey("X-Real-IP"))
-            {
-                string ip_str_ = controllerBase.HttpContext.Request.Headers["X-Real-IP"];
-                stringBuilder.Append($"[{ip_str_}]");
-
-                WebReq.AsyncRequest($"http://ip-api.com/json/{ip_str_}?fields=regionName,city,as", (string url, string jsonresult) =>
-                {
-                    objIPCheckerResult myDeserializedClass = JsonConvert.DeserializeObject<objIPCheckerResult>(jsonresult);
-                    stringBuilder.Append($"[{myDeserializedClass.city}] - ");
-                });
-
-
-            }
-
-            stringBuilder.Append(controllerBase.HttpContext.Request.Method + " - ");
-            stringBuilder.Append(controllerBase.HttpContext.Request.Path + controllerBase.HttpContext.Request.QueryString);
-            G_.logger.NewLine($"{stringBuilder}");
+            controllerBase.HttpContext.LogIp();
         }
     }
 }
